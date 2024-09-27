a2a45029774959f734534f3305df534fef504f8c
Adarsh Dwivedi <dwivediadarsh074@gmail.com>
2024-09-28 03:00:15 +0530

    contact-sectioon

--- index.js
+++ index.js
@@ -3,3 +3,21 @@ window.onscroll = ()=>{
 let header = document.querySelector('.header') ;
 header.classList.toggle('sticky', window.scrollY > 100)
 } ;
+
+
+// SWIPER JS CONFIG // 
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    grabCursor : true , 
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
